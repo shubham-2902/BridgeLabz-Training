d2a1df890e7b34c0bc71afe817693dc026a0a859
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-17 15:13:04 +0530

    [Shubham Kumar Jha]: Added Average.java

---
+++ gcr-codebase/programming-elements/Level 1 Practice Programs/Average.java
@@ -0,0 +1,12 @@
+public class Average {
+    public static void main(String[] args) {
+
+        int marksMaths = 94;
+        int marksPhysics = 95;
+        int marksChemistry = 96;
+
+        double average = (marksMaths + marksPhysics + marksChemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average + "%");
+    }
+}
