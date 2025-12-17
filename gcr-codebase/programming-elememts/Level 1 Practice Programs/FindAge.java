6125dbc62e0af2b461a3edab845cfde41d3936af
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-17 14:42:01 +0530

    [Shubham Kumar Jha]: Added FindAge

---
+++ gcr-codebase/programming-elememts/Level 1 Practice Programs/FindAge.java
@@ -0,0 +1,15 @@
+class FindAge
+{
+public static void main( String [] args)
+{
+
+String name ="Harry";
+int birthyear = 2000;
+int currentYear = 2024;
+// calculating age
+int age = currentYear-birthyear;
+System.out.println(name+"'s"+" age  in "+currentYear+" is "+ age+".");
+
+}
+
+}
