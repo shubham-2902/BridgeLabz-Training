62b9e19b9d2deab42fd944ee2e12796104fba42a
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-17 15:22:53 +0530

    [Shubham Kumar Jha]: Added KilometerToMiles

---
+++ gcr-codebase/programming-elements/Level 1 Practice Programs/KilometerToMiles.java
@@ -0,0 +1,9 @@
+public class KilometerToMiles {
+    public static void main(String[] args) {
+
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;// conversion of km to miles
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
