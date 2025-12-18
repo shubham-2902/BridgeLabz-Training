efd371fd6615949e77494020a178ad217d6c34cb
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-18 16:47:42 +0530

    [Shubham Kumar Jha]: Added SpringSeason

---
+++ gcr-codebase/control-flows/Level-1-problems/SpringSeason.java
@@ -0,0 +1,31 @@
+
+public class SpringSeason {
+
+    public static void main(String[] args) 
+          {
+
+                
+
+        
+        int month;
+        int day;
+
+        // Converting command line arguments to integers
+        month = Integer.parseInt(args[0]);
+        day = Integer.parseInt(args[1]);
+
+        // Checking Spring Season condition
+        boolean isSpringSeason =
+                (month == 3 && day >= 20) ||
+                (month == 4) ||
+                (month == 5) ||
+                (month == 6 && day <= 20);
+
+        // Printing result
+        if (isSpringSeason) {
+            System.out.println("Its a Spring Season");
+        } else {
+            System.out.println("Not a Spring Season");
+        }
+    }
+}
