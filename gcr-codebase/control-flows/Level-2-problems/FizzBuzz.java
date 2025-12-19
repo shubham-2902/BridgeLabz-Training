5f4ecd6965ee33450554c863b3fefdfd085055fa
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-19 06:25:41 +0530

    [Shubham Kumar Jha]: Added FizzBuzz

---
+++ gcr-codebase/control-flows/Level-2-problems/FizzBuzz.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FizzBuzz{
+
+    public static void main(String[] args) {
+
+        // Creating Scanner object
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+           int number = input.nextInt();
+
+        // Checking for positive integer
+        if (number >= 1) {
+
+            // Looping from 1 to number
+            for (int i = 1; i <= number; i++) {
+
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+        input.close();
+    }
+}
