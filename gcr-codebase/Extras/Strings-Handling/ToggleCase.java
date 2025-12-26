84a199ff47dd2dd665553dfcdbe079b2356c929f
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-26 12:42:46 +0530

    [Shubham Kumar Jha]: Added ToggleCase

---
+++ gcr-codebase/Extras/Strings-Handling/ToggleCase.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ToggleCase {
+
+    public static String toggleCase(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z')
+                result += (char)(ch + 32);
+            else if (ch >= 'a' && ch <= 'z')
+                result += (char)(ch - 32);
+            else
+                result += ch;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        System.out.println("Toggled String: " + toggleCase(input));
+    }
+}
