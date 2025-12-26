715ef4e48c35a704f014283715628b10531d20b7
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-26 12:34:41 +0530

    [Shubham Kumar Jha]: Added TimeZonesExample

---
+++ gcr-codebase/Extras/String-TimeClass/TimeZonesExample.java
@@ -0,0 +1,16 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class TimeZonesExample {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        System.out.println("Current Time in GMT : " + gmtTime);
+        System.out.println("Current Time in IST : " + istTime);
+        System.out.println("Current Time in PST : " + pstTime);
+    }
+}
