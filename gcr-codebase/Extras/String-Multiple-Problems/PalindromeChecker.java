863bf56329efb972e3d4b20f2b56d74c8fd74cc6
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-26 12:30:54 +0530

    [Shubham Kumar Jha]: Added PalindromChecker

---
+++ gcr-codebase/Extras/String-Multiple-Problems/PalindromeChecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    // Method to check if a string is a palindrome
+    static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("\\s+", "");
+        int start = 0, end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input))
+            System.out.println("It is a Palindrome");
+        else
+            System.out.println("It is NOT a Palindrome");
+    }
+}
