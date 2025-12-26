60c27e5b17821022464948c5da5c59bf30ab7cb7
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-26 15:44:41 +0530

    [Shubham Kumar Jha]: Added a program for Employee Mgmt

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/Employee.java
@@ -0,0 +1,11 @@
+package com.classandobject.level1;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+
+	}
+
+}
