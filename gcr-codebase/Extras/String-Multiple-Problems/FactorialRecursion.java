e0e41dccd80a48fb08439bd850c094506944c6f2
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2025-12-26 12:22:03 +0530

    [Shubham Kumar Jha]: Added FactorialRecursion

---
+++ gcr-codebase/Extras/String-Multiple-Problems/FactorialRecursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialRecursion {
+
+    // Recursive method to calculate factorial
+    static long factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Factorial of " + num + " is: " + factorial(num));
+    }
+}
