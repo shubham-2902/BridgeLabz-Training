2fa3037fccd62ed1bf46e1e48f253a18c05fcd59
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-05 12:40:45 +0530

    [Shubham Kumar Jha]: Added a program for Undo/Redo Functionality for Text Editor using Doubly Linked List

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/linkedlist/doublylinkedlist/undoredotexteditor/TextState.java
@@ -0,0 +1,14 @@
+package linkedlist.doublylinkedlist.undoredotexteditor;
+
+public class TextState {
+        String content;
+	    TextState prev;
+	    TextState next;
+
+	    TextState(String content) {
+	        this.content = content;
+	        this.prev = null;
+	        this.next = null;
+	    }
+	}
+
