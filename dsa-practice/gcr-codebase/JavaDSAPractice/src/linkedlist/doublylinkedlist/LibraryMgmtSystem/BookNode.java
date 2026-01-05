c699164735bbeb018aa420cd9119ab2ebbe04140
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-05 12:39:12 +0530

    [Shubham Kumar Jha]: Added a program for Library Management System using Doubly Linked List

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/linkedlist/doublylinkedlist/LibraryMgmtSystem/BookNode.java
@@ -0,0 +1,24 @@
+package linkedlist.doublylinkedlist.LibraryMgmtSystem;
+public class BookNode {
+	
+	//Declaration of variables
+    int bookId;
+    String title;
+    String author;
+    String genre;
+    boolean isAvailable;
+
+    BookNode prev;
+    BookNode next;
+
+    //Constructor
+    BookNode(int bookId, String title, String author, String genre, boolean isAvailable) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+        this.prev = null;
+        this.next = null;
+    }
+}
