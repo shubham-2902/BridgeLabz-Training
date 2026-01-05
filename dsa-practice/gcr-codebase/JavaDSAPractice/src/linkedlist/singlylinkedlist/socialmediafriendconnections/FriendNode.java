d8709c56d0e23d68990dbfdd1d28670f2307238b
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-05 12:43:22 +0530

    [Shubham Kumar Jha]: Added a program for Social Media Friend Connections using singly Linked list

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/linkedlist/singlylinkedlist/socialmediafriendconnections/FriendNode.java
@@ -0,0 +1,12 @@
+package linkedlist.singlylinkedlist.socialmediafriendconnections;
+
+
+public class FriendNode {
+    int friendId;
+    FriendNode next;
+
+    public FriendNode(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
