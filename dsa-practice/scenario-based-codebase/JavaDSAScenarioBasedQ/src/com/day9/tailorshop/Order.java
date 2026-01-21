a4a2026276aa2488fdbcab4a03dafdc1b88c4273
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-21 14:33:39 +0530

    [Shubham Kumar Jha]: Added a scenario based program for TailorShop – Sorting Orders by Delivery Deadline (Insertion Sort)

---
+++ dsa-practice/scenario-based-codebase/JavaDSAScenarioBasedQ/src/com/day9/tailorshop/Order.java
@@ -0,0 +1,31 @@
+package com.day9.tailorshop;
+
+public class Order {
+	
+private String customerName;
+private String orderId;
+private String deliveryDate;
+ public Order(String customerName, String orderId, String deliveryDate)
+ {
+	this.customerName= customerName;
+	this.deliveryDate= deliveryDate;
+	this.orderId=orderId;
+ }
+ public String getCustomerName() {
+	 return customerName;
+ }
+ public String getOrderId() {
+	 return orderId;
+ }
+ public String getDeliveryDate() {
+	 return deliveryDate;
+ }
+ 
+ @Override
+ public String toString() {
+	 return customerName+"-> "+orderId+"-> "+deliveryDate;
+ }
+ 
+
+
+}
