43836211a370883c26b36b0d5346728ad5b2ff1f
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-28 17:23:45 +0530

    [Shubham Kumar Jha]: Added a progarm for try with resorces

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/TryWithResources.java
@@ -0,0 +1,20 @@
+package com.exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("info.txt"))) {
+
+            String firstLine = reader.readLine();
+            System.out.println(firstLine);
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
