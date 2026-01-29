1a58f8877824e681f172dbb90fd5da6e0a88a895
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-29 13:30:20 +0530

    [Shubham Kumar Jha]: Added a program for logic class of StringUtils

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/JUnit/StringUtils.java
@@ -0,0 +1,25 @@
+package com.JUnit;
+public class StringUtils {
+
+    public String reverse(String str) {
+        if (str == null) {
+            return null;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        String reversed = reverse(str);
+        return str.equals(reversed);
+    }
+
+    public String toUpperCase(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.toUpperCase();
+    }
+}
