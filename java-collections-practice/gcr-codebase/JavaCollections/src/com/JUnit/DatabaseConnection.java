9ea715d1a6c1cce94a1e10ed5886ebd6eb16eda5
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-29 13:26:55 +0530

    [Shubham Kumar Jha]: Added a program for logic class of databaseConnection

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/JUnit/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package com.JUnit;
+public class DatabaseConnection {
+
+    private boolean connected;
+
+    public void connect() {
+        connected = true;
+    }
+
+    public void disconnect() {
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
