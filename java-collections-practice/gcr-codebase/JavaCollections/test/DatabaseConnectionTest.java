1852421c457cfb404405d34923571dda5c73c9f1
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-01-29 14:13:36 +0530

    [Shubham Kumar Jha]: Added a program for test class for DatabaseConnectionTest

---
+++ java-collections-practice/gcr-codebase/JavaCollections/test/DatabaseConnectionTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.JUnit.DatabaseConnection;
+
+class DatabaseConnectionTest {
+
+    private DatabaseConnection db;
+
+    @BeforeEach
+    void setUp() {
+        db = new DatabaseConnection();
+        db.connect();
+    }
+
+    @AfterEach
+    void tearDown() {
+        db.disconnect();
+    }
+
+    @Test
+    void testConnectionEstablished() {
+        assertTrue(db.isConnected());
+    }
+
+    @Test
+    void testConnectionStillActiveDuringTest() {
+        assertTrue(db.isConnected());
+    }
+}
